23975f3b1259f4e44fbc77955af4fb757e9cc556
Nguyễn Duy Vũ <vund.0709@gmail.com>
2021-11-13 19:00:47 +0700

    Import quiz, lesson, topic tu file excel

--- src/app/controllers/LessonController.js
+++ src/app/controllers/LessonController.js
@@ -2,6 +2,7 @@ const path = require('path')
 const Lesson = require('../model/LessonModel')
 const xlsx = require('xlsx');
 const Topic = require('../model/TopicModel')
+const Quiz = require('../model/QuizModel')
 
 class LessonController {
 
@@ -9,35 +10,36 @@ class LessonController {
     index(req, res) {
         res.render('add_page')
     }
-
-
-
     importLessonFromExcelFile(req, res, next) {
         const workbook = xlsx.readFile(req.file.path);
         var sheet_name_list = workbook.SheetNames;
-        if (sheet_name_list.length < 2) {
+        if (sheet_name_list.length < 3) {
             res.send('<h1>File sai định dạng</h1>');
             return;
         }
-        let lessonSheet = workbook.Sheets[workbook.SheetNames[0]]
+
 
         try {
-            const lesson = Lesson({
-                title: lessonSheet[`A${2}`].v,
-                totalTopic: parseInt(lessonSheet[`B${2}`].v),
-            })
-            lesson.save().then((newLesson) => {
+            //import lesson
+            let lessonSheet = workbook.Sheets[workbook.SheetNames[0]]
+            var xlData = xlsx.utils.sheet_to_json(lessonSheet);
+            Lesson({
+                title: xlData[0].title,
+                totalTopic: xlData[0].totalTopic,
+            }).save().then((newLesson) => {
                 let topicSheet = workbook.Sheets[workbook.SheetNames[1]]
-                var range = xlsx.utils.decode_range(topicSheet['!ref']);
-                var num_rows = range.e.r - range.s.r + 1;
-
-                for (var i = 2; i <= num_rows; i++) {
-                    const topic = Topic({
+                xlData = xlsx.utils.sheet_to_json(topicSheet);
+                for (var i in xlData) {
+                    console.log({
                         lessonId: newLesson._id,
-                        title: topicSheet[`A${i}`].v,
-                        content: topicSheet[`B${i}`].v
+                        title: xlData[i].title,
+                        content: xlData[i].content
                     })
-                    topic.save().then((newTopic) => {
+                    Topic({
+                        lessonId: newLesson._id,
+                        title: xlData[i].title,
+                        content: xlData[i].content
+                    }).save().then((newTopic) => {
                         return res.status(201).json({
                             success: true,
                             message: 'Create successfully',
@@ -51,18 +53,48 @@ class LessonController {
                         });
                         return;
                     })
-
                 }
-            })
-                .catch((error) => {
-                    console.log(error);
-                    res.status(500).json({
-                        success: false,
-                        message: 'Create failed. Please try again.',
-                        error: error.message,
-                    });
-                    return;
+
+                //import quiz:
+                const quizSheet = workbook.Sheets[workbook.SheetNames[2]]
+                xlData = xlsx.utils.sheet_to_json(quizSheet);
+                for (var i in xlData) {
+                    Quiz({
+                        lessonId: newLesson._id,
+                        STT: xlData[i].STT,
+                        question: xlData[i].question,
+                        answer: [
+                            xlData[i].answerA,
+                            xlData[i].answerB,
+                            xlData[i].answerC,
+                            xlData[i].answerD
+                        ],
+                        correctAnswer: xlData[i].correctAnswer,
+
+                    }).save().then((newTopic) => {
+                        return res.status(201).json({
+                            success: true,
+                            message: 'Create successfully',
+                        })
+                    }).catch((error) => {
+                        console.log(error);
+                        res.status(500).json({
+                            success: false,
+                            message: 'Create failed. Please try again.',
+                            error: error.message,
+                        });
+                        return;
+                    })
+                }
+            }).catch((error) => {
+                console.log(error);
+                res.status(500).json({
+                    success: false,
+                    message: 'Create failed. Please try again.',
+                    error: error.message,
                 });
+                return;
+            });
 
 
 
