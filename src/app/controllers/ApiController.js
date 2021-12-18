349051db40c1dd7ac037d717ceba5bfeb7ec74b6
Nguyễn Duy Vũ <vund.0709@gmail.com>
2021-12-18 21:56:31 +0700

    Fix bug

--- src/app/controllers/ApiController.js
+++ src/app/controllers/ApiController.js
@@ -113,7 +113,7 @@ class ApiController {
         Lesson.find({}).then(lslist => {
             var listData = [];
             for (var ls of lslist) {
-              await Topic.find({lessonId: ls._id}).then(topicList => {
+                Topic.find({ lessonId: ls._id }).then(topicList => {
                     var lessonAll = new LessonAll(ls.id, ls.title, ls.totalTopic, topicList, null)
                     listData.push(lessonAll)
 
@@ -131,154 +131,154 @@ class ApiController {
                 data: listData
             })
 
-    }).catch(e => res.json({
-        status: false,
-        message: e.message,
-        code: 404
-    }))
-     
-    }
-
-
-//get Program:
-getProgram(req, res, next) {
-    var listData = []
-    Program.find({}).then(program => {
-        for (var pr of program) {
-            // var data = Buffer.from(pr.image.data, "binary").toString("base64");
-            var programMd = new ProgramMD(pr._id, pr.name, pr.image)
-            listData.push(programMd)
-        }
-        res.json(listData)
-
-    }).catch(e => res.json({
-        status: false,
-        message: e.message,
-        code: 404
-    }))
-}
-
-//get program detail by id:
-getProgramDetail(req, res, next) {
-    if (req.query.programId == null) {
-        res.json({
-            message: 'Cần truyền param programId',
-            status: false
-        })
-        return;
-    }
-    ProgramDetail.find({ programId: req.query.programId }).then(programDetail => {
-        res.json(programDetail)
-    }).catch(e => res.json({
-        isSuccess: false,
-        message: e.message,
-        code: 404
-    }))
-}
-
-    //get all in program
-    async getAllInProgram(req, res, next) {
-    var program = await Program.find({})
-    var listData = [];
-    for (var pr of program) {
-        const programDetail = await ProgramDetail.find({ programId: pr._id })
-        var program = new ProgramWithDetail(pr._id, pr.name, programDetail, pr.image)
-        listData.push(program)
-    }
-    res.json(listData)
-}
-
-//get score daily score by user id
-getDailyScore(req, res) {
-    if (req.query.userId == null || req.query.date == null) {
-        res.json({
-            isSuccess: false,
-            message: 'Cần truyền userId, date',
+        }).catch(e => res.json({
+            status: false,
+            message: e.message,
             code: 404
-        })
-        return
+        }))
+
     }
-    Process.find({ userId: req.query.userId, lastModify: req.query.date }).then(users => {
-        var sumScore = 0
-        for (var i of users) {
-            sumScore += Number(i.quizMarked)
+
+
+    //get Program:
+    getProgram(req, res, next) {
+        var listData = []
+        Program.find({}).then(program => {
+            for (var pr of program) {
+                // var data = Buffer.from(pr.image.data, "binary").toString("base64");
+                var programMd = new ProgramMD(pr._id, pr.name, pr.image)
+                listData.push(programMd)
+            }
+            res.json(listData)
+
+        }).catch(e => res.json({
+            status: false,
+            message: e.message,
+            code: 404
+        }))
+    }
+
+    //get program detail by id:
+    getProgramDetail(req, res, next) {
+        if (req.query.programId == null) {
+            res.json({
+                message: 'Cần truyền param programId',
+                status: false
+            })
+            return;
         }
-        res.json({
-            isSuccess: true,
-            code: 200,
-            message: "success",
-            score: sumScore,
-            userId: req.query.userId,
-            date: req.query.date
-        })
-    }).catch(e => {
-        res.json({
+        ProgramDetail.find({ programId: req.query.programId }).then(programDetail => {
+            res.json(programDetail)
+        }).catch(e => res.json({
             isSuccess: false,
             message: e.message,
             code: 404
-        })
-    })
-}
-
-    //get 1 week score:
-    async getMarkProfile(req, res) {
-    if (req.query.userId == null) {
-        res.json({
-            isSuccess: false,
-            message: 'Cần truyền userId',
-            code: 404
-        })
-        return
+        }))
     }
-    var today = new Date();
-    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    var now = new Date(date)
-    var nowToMilisen = now.getTime()
-    var listData = []
-    for (var i = 0; i < 7; i++) {
-        var milisecond = 1000 * 60 * 60 * 24 * i
-        var dayWork = nowToMilisen - milisecond //tính khoảng cách hiện tại tới ngày thứ 'i'
-        var day = new Date(dayWork) // trả về datetime: yyyy-MM-dd hh:mm:ss
-        var dayFomarted = day.getUTCFullYear() + "/" + (day.getUTCMonth() + 1) + "/" + day.getUTCDate(); // format datetime: yyyy-MM-dd
-        var dayReturn = day.getUTCDate() + '/' + (day.getUTCMonth() + 1);
-        try {
-            var process = await Process.find({ userId: req.query.userId, lastModify: dayFomarted })
-            console.log('date: ' + dayFomarted)
-            console.log(process)
-            if (process.length > 0) {
-                var sumScore = 0
-                for (var j of process) {
-                    sumScore += Number(j.quizMarked)
-                }
-                console.log(sumScore)
-                listData.push({
-                    date: dayReturn,
-                    mark: sumScore
-                })
-            } else {
-                listData.push({
-                    date: dayReturn,
-                    mark: 0
-                })
+
+    //get all in program
+    async getAllInProgram(req, res, next) {
+        var program = await Program.find({})
+        var listData = [];
+        for (var pr of program) {
+            const programDetail = await ProgramDetail.find({ programId: pr._id })
+            var program = new ProgramWithDetail(pr._id, pr.name, programDetail, pr.image)
+            listData.push(program)
+        }
+        res.json(listData)
+    }
+
+    //get score daily score by user id
+    getDailyScore(req, res) {
+        if (req.query.userId == null || req.query.date == null) {
+            res.json({
+                isSuccess: false,
+                message: 'Cần truyền userId, date',
+                code: 404
+            })
+            return
+        }
+        Process.find({ userId: req.query.userId, lastModify: req.query.date }).then(users => {
+            var sumScore = 0
+            for (var i of users) {
+                sumScore += Number(i.quizMarked)
             }
-        } catch (e) {
+            res.json({
+                isSuccess: true,
+                code: 200,
+                message: "success",
+                score: sumScore,
+                userId: req.query.userId,
+                date: req.query.date
+            })
+        }).catch(e => {
             res.json({
                 isSuccess: false,
                 message: e.message,
                 code: 404
             })
-        }
+        })
     }
 
-    res.json({
-        isSuccess: true,
-        code: 200,
-        message: "success",
-        userId: req.query.userId,
-        data: listData,
-    })
+    //get 1 week score:
+    async getMarkProfile(req, res) {
+        if (req.query.userId == null) {
+            res.json({
+                isSuccess: false,
+                message: 'Cần truyền userId',
+                code: 404
+            })
+            return
+        }
+        var today = new Date();
+        var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        var now = new Date(date)
+        var nowToMilisen = now.getTime()
+        var listData = []
+        for (var i = 0; i < 7; i++) {
+            var milisecond = 1000 * 60 * 60 * 24 * i
+            var dayWork = nowToMilisen - milisecond //tính khoảng cách hiện tại tới ngày thứ 'i'
+            var day = new Date(dayWork) // trả về datetime: yyyy-MM-dd hh:mm:ss
+            var dayFomarted = day.getUTCFullYear() + "/" + (day.getUTCMonth() + 1) + "/" + day.getUTCDate(); // format datetime: yyyy-MM-dd
+            var dayReturn = day.getUTCDate() + '/' + (day.getUTCMonth() + 1);
+            try {
+                var process = await Process.find({ userId: req.query.userId, lastModify: dayFomarted })
+                console.log('date: ' + dayFomarted)
+                console.log(process)
+                if (process.length > 0) {
+                    var sumScore = 0
+                    for (var j of process) {
+                        sumScore += Number(j.quizMarked)
+                    }
+                    console.log(sumScore)
+                    listData.push({
+                        date: dayReturn,
+                        mark: sumScore
+                    })
+                } else {
+                    listData.push({
+                        date: dayReturn,
+                        mark: 0
+                    })
+                }
+            } catch (e) {
+                res.json({
+                    isSuccess: false,
+                    message: e.message,
+                    code: 404
+                })
+            }
+        }
 
-}
+        res.json({
+            isSuccess: true,
+            code: 200,
+            message: "success",
+            userId: req.query.userId,
+            data: listData,
+        })
+
+    }
 
 }
 
